f428873627aeaecff6840cbae82606eb7f344b3d
Carlos Guillen <carlos.guillen@egoargentina.com>
2019-10-01 16:16:43 -0300

    ADD Librerias comunes al proyecto

---
+++ OPB-PROYECT/projects/obp-common/src/lib/obp-common.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { TopbarComponent } from './topbar/topbar.component';
+// import { ObpCommonComponent } from './obp-common.component';
+
+
+
+@NgModule({
+  declarations: [TopbarComponent],
+  imports: [
+  ],
+  exports: [TopbarComponent]
+})
+export class ObpCommonModule { }
---
+++ OPB-PROYECT/projects/obp-common/src/public-api.ts
@@ -0,0 +1,10 @@
+/*
+ * Public API Surface of obp-common
+ */
+
+// export * from './lib/obp-common.service';
+// export * from './lib/obp-common.component';
+export * from './lib/obp-common.module';
+
+// componentes
+export * from './lib/topbar/topbar.component';
